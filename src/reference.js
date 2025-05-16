67abc836a7f8d9a135a10a692c88a86fd8be9988
andrew0samir <andrewsamir317@gmail.com>
2025-05-16 21:43:58 +0300

    fetched data using useEffect hook

--- src/reference.js
+++ src/reference.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const tempMovieData = [
     {
@@ -50,17 +50,10 @@ const tempWatchedData = [
 const average = (arr) =>
     arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
-const KEY = 'd476248b';
-
 export default function App() {
-    const [movies, setMovies] = useState([]);
-    const [watched, setWatched] = useState([]);
+    const [movies, setMovies] = useState(tempMovieData);
+    const [watched, setWatched] = useState(tempWatchedData);
 
-
-    useEffect(function () {
-        fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
-            .then(res => res.json()).then(data => setMovies(data.Search))
-    }, []);
 
     return (
         <>
